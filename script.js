5df5216749e934c67e4d491379ee017a663a459d
Tomoki Yoshioka <tomoking1004@icloud.com>
2019-03-09 16:43:55 -0800

    Changed

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function successCallback(stream) {
 function errorCallback(err) {
   alert(err);
 };
+
+// 保存ボタン
+var button = document.getElementById('button');
+// 画像を表示する要素
+var gallary = document.getElementById('gallary');
+ 
+function displaySnapshot() {
+  var snapshot = new Image();
+ 
+  snapshot.src = canvas.toDataURL('image/png');
+    snapshot.onload = function(){
+      snapshot.width = snapshot.width / 2;
+      snapshot.height = snapshot.height / 2;
+      gallary.appendChild(snapshot);
+    }
+}
+ 
+// 保存ボタンを押したら実行
+button.addEventListener('click', displaySnapshot);
